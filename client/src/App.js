c79a8ab5a7125c52f185ad0375c80dfa9230d8bf
shubhdeep123 <shubhdeepsharma213@gmail.com>
2021-07-18 15:56:21 +0530

    Setting routes and implemented sementic UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import MenuBar from "./components/MenuBar";
+import "semantic-ui-css/semantic.min.css";
+import { Container } from "semantic-ui-react";
+import "./App.css";
 
 function App() {
   return (
-    <h1>App is running</h1>
+    <Container>
+      <Router>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Router>
+    </Container>
   );
 }
 
